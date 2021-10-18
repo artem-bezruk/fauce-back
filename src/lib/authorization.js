d2abb1d7fc222854d3bffa480b7ba4e15b3fdf07
Artem <artem.web.developer.it@gmail.com>
2021-10-18 00:26:07 +0000

    Adding HTTP basic auth

--- src/lib/authorization.js
+++ src/lib/authorization.js
@@ -1,36 +1,30 @@
-exports.handler = function (event, context, callback) {
-    var authorizationHeader = event.headers.Authorization
-    if (!authorizationHeader) return callback('Unauthorized')
-    var encodedCreds = authorizationHeader.split(' ')[1]
-    var plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':')
-    var username = plainCreds[0]
-    var password = plainCreds[1]
-    if (!(username === process.env.HTTP_BASIC_AUTH_USER && password === process.env.HTTP_BASIC_AUTH_USER)) return callback('Unauthorized')
-    var authResponse = buildAllowAllPolicy(event, username)
-    callback(null, authResponse)
-  }
-  function buildAllowAllPolicy (event, principalId) {
-    var apiOptions = {}
-    var tmp = event.methodArn.split(':')
-    var apiGatewayArnTmp = tmp[5].split('/')
-    var awsAccountId = tmp[4]
-    var awsRegion = tmp[3]
-    var restApiId = apiGatewayArnTmp[0]
-    var stage = apiGatewayArnTmp[1]
-    var apiArn = 'arn:aws:execute-api:' + awsRegion + ':' + awsAccountId + ':' +
-      restApiId + '/' + stage + '/*/*'
+buildAllowAllPolicy = (event, principalId) => {
     const policy = {
-      principalId: principalId,
-      policyDocument: {
+        principalId: principalId,
+        policyDocument: {
         Version: '2012-10-17',
         Statement: [
-          {
+            {
             Action: 'execute-api:Invoke',
             Effect: 'Allow',
-            Resource: [apiArn]
-          }
-        ]
-      }
+            Resource: event.methodArn
+            }]
+        }
     }
-    return policy
-  }  
+    return policy;
+}
+exports.handler = async (event, context, callback) => {
+    var authorizationHeader = event.headers.Authorization;  
+    if (!authorizationHeader) context.fail('Unauthorized');
+    var encodedCreds = authorizationHeader.split(' ')[1];
+    console.log(JSON.stringify(encodedCreds));
+    var plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':');
+    console.log(JSON.stringify(plainCreds));
+    var username = plainCreds[0];
+    var password = plainCreds[1];
+    console.log(`username = ${username}, password=${password}`);
+    if (!(username === process.env.HTTP_BASIC_AUTH_USER && password === process.env.HTTP_BASIC_AUTH_USER)) 
+        context.fail('Unauthorized');
+    const authResponse = buildAllowAllPolicy(event, username)
+    return authResponse;
+  }
