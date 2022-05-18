b11c1ff042abd25eaae3f4ac49cba4d9bb5aaa55
Artem <artem.web.developer.it@gmail.com>
2022-05-18 15:47:46 +0000

    Merge branch 'alfajores' into 'develop'

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var utils = require('../lib/utils');
-describe('utils', () => {
+describe('utils', async () => {
   describe('createHttpResponse()', () => {
     it('should return valid API Gateway HTTP payload ', () => {
       const expected = {
@@ -25,9 +25,9 @@ describe('utils', () => {
     });
   });
   describe('getCurrentBlockNumber()', () => {
-    it('should successfully return current Alfajores block number', () => {
-      const result = utils.getCurrentBlockNumber();
-      assert(result);
+    it('should successfully return current Alfajores block number', async () => {
+      const result = await utils.getCurrentBlockNumber();
+      assert(result > 0);
     });
   });  
   describe('sortByRequestedBlock()', () => {
